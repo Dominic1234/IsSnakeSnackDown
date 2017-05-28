a46d379402fe4f98b22964c83bcbcb86939868ab
sharmadpk@gmail.com <sharmadpk@gmail.com>
2017-05-28 09:42:02 +0530

    Added debug

--- main.c
+++ main.c
@@ -1,27 +1,39 @@
 #include<stdio.h>
 #include<stdlib.h>
+
+int debug = 0;
+
 int main()
 {
 	int t, n, a, b, count1, count2, condition, scelly, ecelly;
 	char input;
 	int *cell[2];
 	scanf("%d", &t);
+	if (debug) printf ("#Tests = %d\n", t);
 	for(a = 0; a < t; a++)
 	{
 		condition = 1;
 		scanf("%d", &n);
+		if (debug) printf ("TC%d has %d columns\n", a, n);
 		for(b = 0; b < 2; b++)
 			cell[b] = (int*)malloc(n * sizeof(int));
+		if (debug) printf ("\tReading inputs\n");
 		for(count1 = 0; count1 < 2; count1++)
 		{
+			printf("\t");
 			for(count2 = 0; count2 < n; count2++)
 			{
-				scanf("%c", &input);
+				do {
+					scanf("%c", &input);
+				} while (input == '\r' || input == '\n');
 				if(input == '#') cell[count1][count2] = 1;
 				else cell[count1][count2] = 0;
+				if (debug) printf ("%c-%d, ", input, cell[count1][count2]);
 			}
+			if(debug) printf("\n");
 		}
 
+		if (debug)
 		for(count1 = 0; count1 < 2; count1++)
 				{
 					for(count2 = 0; count2 < n; count2++)
