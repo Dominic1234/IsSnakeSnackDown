bf9f58d6cec08d7a146ba0aa3f309b0ee92793c8
Dominic1234 <dhruv.sharma.017@gmail.com>
2017-05-28 09:27:06 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,55 +1,77 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<string.h>
-
-int debug = 0;
-
-int main(int argc, char **argv)
+int main()
 {
-	int a, b;
-	int tc;//testcases
-	int len;//Length of string
-	int count = 0;
-
-
-	scanf("%d", &tc);
-	if (debug) printf ("TC=%d\n", tc);
-	for( a = 0; a < tc; a++)
+	int t, n, a, b, count1, count2, condition, scelly, ecelly;
+	char input;
+	int *cell[2];
+	scanf("%d", &t);
+	for(a = 0; a < t; a++)
 	{
-		count = 0;
-		scanf("%d", &len);
-		char snake;
-		for(b = 0; b < len; b++)
+		condition = 1;
+		scanf("%d", &n);
+		for(b = 0; b < 2; b++)
+			cell[b] = (int*)malloc(n * sizeof(int));
+		for(count1 = 0; count1 < 2; count1++)
 		{
-			scanf("%c", &snake);
-
-			if(snake == 'H' && count == 0)
+			for(count2 = 0; count2 < n; count2++)
 			{
-				count = 1;
-			}
-			else if(snake == 'H' && count > 0)
-			{
-				count = 2;
-			}
-			else if(snake == 'T' && count == 1)
-			{
-				count = 0;
-			}
-			else if(snake == 'T' && count != 1)
-			{
-				count = 2;
+				scanf("%c", &input);
+				if(input == '#') cell[count1][count2] = 1;
+				else cell[count1][count2] = 0;
 			}
 		}
-			if(count == 1)
-			{
-				printf("Invalid\n");
-			}
-			else if(count == 0)
-				printf("Valid\n");
-			else{
-				printf("Invalid\n");
-			}
-	}
 
-	return 0;
+		for(count1 = 0; count1 < 2; count1++)
+				{
+					for(count2 = 0; count2 < n; count2++)
+					{
+						printf("%d", cell[count1][count2]);
+					}
+					printf("\n");
+				}
+
+				for(count2 = 0; count2 < n; count2++)
+					{
+					if(cell[1][count2] == 1 || cell[0][count2] == 1)
+						{
+							//scell{x,y} stands for the x,y of the start cell
+							scelly = count2;
+							break;
+						}
+					}
+
+				for(count2 = n; count2 > scelly; count2--)
+					{
+					if(cell[0][count2] == 1 || cell[1][count2] == 1)
+						{
+							//ecell{x,y} stands for x,y of end cell
+							ecelly = count2;
+							break;
+						}
+					}
+			for(count2 = scelly; count2 < ecelly; count2++)
+				{
+					if(cell[0][count2] == 0)
+					{
+						if(cell[1][count2] == 0) condition = 0;
+					}
+					if (cell[0][count2+1] == 0 && cell[1][count2] == 0)
+					condition = 0;
+					if(cell[1][count2+1] == 0 && cell[0][count2] == 0)condition = 0;
+					if(cell[0][count2] == 1)
+										{
+											if(cell[1][count2] == 1)
+											{
+												if(cell[0][count2+1] == 1 && cell[1][count2+1] == 0)
+												{
+
+												}
+											}
+										}
+				}
+			if(condition == 1) printf("yes\n");
+			else printf("no\n");
 	}
+	return 0;
+}
